b20467f01485471a533c232283053afa21bfa107
yinonbaron <yinon.baron@gmail.com>
2019-06-17 10:23:51 +0300

    updated 2017 analysis

---
+++ processing_itai/Shannon.py
@@ -0,0 +1,57 @@
+# This script will calculate Shannon entropy from a MSA.
+
+# Dependencies:
+
+# Biopython, Matplotlib [optionally], Math
+
+"""
+Shannon's entropy equation (latex format):
+    H=-\sum_{i=1}^{M} P_i\,log_2\,P_i
+    Entropy is a measure of the uncertainty of a probability distribution (p1, ..... , pM)
+    https://stepic.org/lesson/Scoring-Motifs-157/step/7?course=Bioinformatics-Algorithms&unit=436
+    Where, Pi is the fraction of nuleotide bases of nuleotide base type i,
+    and M is the number of nuleotide base types (A, T, G or C)
+    H ranges from 0 (only one base/residue in present at that position) to 4.322 (all 20 residues are equally
+    represented in that position).
+    Typically, positions with H >2.0 are considerered variable, whereas those with H < 2 are consider conserved.
+    Highly conserved positions are those with H <1.0 (Litwin and Jores, 1992).
+    A minimum number of sequences is however required (~100) for H to describe the diversity of a protein family.
+"""
+import os
+import sys
+import warnings
+import traceback
+##################################################################
+# Function to calcuate the Shannon's entropy per alignment column
+# H=-\sum_{i=1}^{M} P_i\,log_2\,P_i (http://imed.med.ucm.es/Tools/svs_help.html)
+# Gaps and N's are included in the calculation
+##################################################################
+
+def shannon_entropy(list_input):
+    """Calculate Shannon's Entropy per column of the alignment (H=-\sum_{i=1}^{M} P_i\,log_2\,P_i)"""
+
+    import math
+    unique_base = set(list_input)
+    M   =  len(list_input)
+    entropy_list = []
+    # Number of residues in column
+    for base in unique_base:
+        n_i = list_input.count(base) # Number of residues of type i
+        P_i = n_i/float(M) # n_i(Number of residues of type i) / M(Number of residues in column)
+        entropy_i = P_i*(math.log(P_i,2))
+        entropy_list.append(entropy_i)
+
+    sh_entropy = -(sum(entropy_list))
+
+    return sh_entropy
+
+
+def shannon_entropy_list_msa(alignment):
+    """Calculate Shannon Entropy across the whole MSA"""
+
+    shannon_entropy_list = []
+    for col_no in range(len(list(alignment[0]))):
+        list_input = list(alignment[:, col_no])
+        shannon_entropy_list.append(shannon_entropy(list_input))
+
+    return shannon_entropy_list
